d2c4df010da63a98a0dfde32f6899900ee2d38a4
inglec <inglec@tcd.ie>
2018-10-31 19:22:09 +0000

    Update formatter

--- scripts/formatter.py
+++ scripts/formatter.py
@@ -1,57 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+import sys
 
-This is a temporary script file.
-"""
-import re
+# Command line args
+hashes = open(sys.argv[1]).readlines()
+broken = open(sys.argv[2]).readlines()
 
+# Create dict from hashes to shares
+hashesToShares = {}
+for entry in hashes:
+    split = entry.strip().split(':')
+    share = split[0]
+    hash = split[1]
 
-def formatSha512(potfile,hashfile):
-    count=0
-    x = []
-    y = []
-    z = []
-    with open("..\\infernoballs\\broken\\sha512.broken") as file1:
-        for l in file1:
-            x.append(l.strip())
-            #print(x)
-    with open("infernoball1.hashes") as file2:
-        for l in file2:
-            y.append(l.strip())
-            #print(y)
-    
-    for hashes in x:
-        aPswdHash = hashes.split(':')[0]
-        escPswdHash = re.escape(aPswdHash.split('$')[4])
-        #print('hash is ----->',escPswdHash)
-        for shr in y:
-            #print(shr)
-            escShrHash = re.escape((shr.split(':')[1]).split('$')[4])
-            #print(escShrHash) 
-            #if(re.match(escPswdHash,escShrHash)):
-            if(escShrHash.find(escPswdHash)!=-1):
-                #print(True)
-                count += 1
-                #print(count)
-                z.append(shr.split(':')[0]+':'+hashes.split(':')[1])#.append(':').append(hashes.split(':')[1])
-                break
-                
-                
-    print (count)
-    print(z)
-    return z
+    hashesToShares[hash] = share
 
+for entry in broken:
+    split = entry.strip().split(':')
+    hash = split[0]
+    word = split[1]
 
-def main():
-    potfile = 1
-    hashfile =2
-    matchArr = formatSha512(potfile,hashfile)
-    with open('sha512_.broken', 'w') as f:
-        for item in matchArr:
-            f.write("%s\n" % item)
-    
-
-if __name__== "__main__":
-    main()
-    
+    share = hashesToShares[hash]
+    print(share + ":" + word)
