951f4c03d5340e0fe2c5cfb25d9a6f64934dea91
inglec <inglec@tcd.ie>
2018-11-02 11:03:51 +0000

    Fix 5char script

--- scripts/generate5.py
+++ scripts/generate5.py
@@ -7,11 +7,17 @@ digits = '0123456789'
 
 charset = letters + digits
 
+def hasNumber(word):
+    for c in word:
+        if c in digits:
+            return True
+    return False
+
 def hasVowel(word):
     for c in word:
         if c in vowels:
-            return true
-    return false
+            return True
+    return False
 
 # Modify this function to reduce the size of the wordlist.
 def satisfiesConstraint(word):
@@ -21,7 +27,7 @@ def next(prefix, remaining):
 	if remaining == 0:
         # We may only want to retain certain words.
 		if satisfiesConstraint(prefix):
-            print(prefix)
+                    print(prefix)
 	else:
 		for c in charset:
 			next(prefix + c, remaining - 1)
